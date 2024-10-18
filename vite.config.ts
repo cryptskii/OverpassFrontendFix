a03fb1284d4381999a8c3bc3fbce053f5a99a5da
Cryptskii <47649969+cryptskii@users.noreply.github.com>
2024-10-17 22:53:35 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,25 @@
-import { PluginOption, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
-import { analyzer } from 'vite-bundle-analyzer'
+import { resolve } from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-const addAnalyzer: PluginOption[] = process.env.ANALYZE === '1' ? [analyzer()] : []
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ...addAnalyzer],
-  root: 'public', // Set the root to the public directory
+  plugins: [
+    react(),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+      },
+    }),
+  ],
+  root: '.',
   publicDir: 'public',
-  server: {
-    port: 3000,
-  },
-  base: '',
-  optimizeDeps: {
-    include: ['bn.js'],
-    esbuildOptions: {
-      target: 'es2020',
-    },
-  },
   build: {
-    target: 'es2020',
-    outDir: '../dist', // Output to the dist folder in the project root
+    outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'public/index.html'),
+        main: resolve(__dirname, 'index.html'),
       },
     },
   },
